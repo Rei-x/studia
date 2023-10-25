f52950157592e340635c38485028151331323045
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-10-25 13:17:55 +0200

    fix(PP): simplify palindrome function

--- PP/cwiczenia/01/main.ml
+++ PP/cwiczenia/01/main.ml
@@ -10,13 +10,14 @@ let rec replicate (x, n) = if n = 0 then [] else x :: replicate (x, n - 1)
 let rec sqrList xs =
   if xs = [] then [] else (List.hd xs * List.hd xs) :: sqrList (List.tl xs)
 
-let rec palindrome xs =
-  if List.length xs = 0 || List.length xs = 1 then true
-  else
-    List.hd xs = List.hd (List.rev xs)
-    && palindrome
-         (List.filter
-            (fun x -> x != List.hd xs)
-            (List.filter (fun x -> x != List.hd (List.rev xs)) xs))
+let palindrome xs =
+  (* if List.length xs = 0 || List.length xs = 1 then true
+     else
+       List.hd xs = List.hd (List.rev xs)
+       && palindrome
+            (List.filter
+               (fun x -> x != List.hd xs)
+               (List.filter (fun x -> x != List.hd (List.rev xs)) xs)) *)
+  List.rev xs = xs
 
 let rec listLength xs = if xs = [] then 0 else 1 + listLength (List.tl xs)
