6cfc483a5779337fa093909045797d8e7193f80d
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-12-03 14:24:15 +0100

    fix(PP): fix scala compilation

--- PP/cwiczenia/03/cwiczenia03.scala
+++ PP/cwiczenia/03/cwiczenia03.scala
@@ -6,7 +6,7 @@ def uncurry3[A, B, C, D](f: A => B => C => D): (A, B, C) => D = { (a, b, c) =>
   f(a)(b)(c)
 }
 
-def sumProd(xs: List[Int]): (Int, Int) = {
+def sumProd2(xs: List[Int]): (Int, Int) = {
   xs.foldLeft(0, 1)((accumulator, h) =>
     (accumulator._1 + h, accumulator._2 * h)
   )
