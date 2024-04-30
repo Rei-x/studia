4409d334108ddb6648880634ba5702532fc47a3d
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2024-04-30 06:52:44 +0200

    chore: styling

--- JezykiSkryptowe/laby06/laby06/main.py
+++ JezykiSkryptowe/laby06/laby06/main.py
@@ -4,18 +4,7 @@ from laby06.SSHLogJournal import SSHLogJournal
 
 
 journal = SSHLogJournal()
-# Dec 10 06:55:46 LabSZ sshd[24200]: reverse mapping checking getaddrinfo for ns.marryaldkfaczcz.com [173.234.31.186] failed - POSSIBLE BREAK-IN ATTEMPT!
-# Dec 10 06:55:46 LabSZ sshd[24200]: Invalid user webmaster from 173.234.31.186
-# Dec 10 06:55:46 LabSZ sshd[24200]: input_userauth_request: invalid user webmaster [preauth]
-# Dec 10 06:55:46 LabSZ sshd[24200]: pam_unix(sshd:auth): check pass; user unknown
-# Dec 10 06:55:46 LabSZ sshd[24200]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=173.234.31.186
-# Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from 173.234.31.186 port 38926 ssh2
-# Dec 10 06:55:48 LabSZ sshd[24200]: Connection closed by 173.234.31.186 [preauth]
-# Dec 10 07:02:47 LabSZ sshd[24203]: Connection closed by 212.47.254.145 [preauth]
-# Dec 10 07:07:38 LabSZ sshd[24206]: Invalid user test9 from 52.80.34.196
-# Dec 10 07:07:38 LabSZ sshd[24206]: input_userauth_request: invalid user test9 [preauth]
-# Dec 10 07:07:38 LabSZ sshd[24206]: pam_unix(sshd:auth): check pass; user unknown
-# Dec 10 07:07:38 LabSZ sshd[24206]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=ec2-52-80-34-196.cn-north-1.compute.amazonaws.com.cn
+
 journal.append(
     "Dec 10 06:55:46 LabSZ sshd[24200]: reverse mapping checking getaddrinfo for ns.marryaldkfaczcz.com [173.234.31.186] failed - POSSIBLE BREAK-IN ATTEMPT!"
 )
@@ -58,7 +47,6 @@ users = [SSHUser("webmaster", None), SSHUser("test9", None)]
 log_entries = journal.get_logs_by_type(MessageType.INVALID_PASSWORD)
 
 two_lists = users + log_entries
-
 
 for item in two_lists:
     print(item)
