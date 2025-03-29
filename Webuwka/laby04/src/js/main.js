f8550e352b0a3ecf798345f48ebbc3fefe2998e5
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2025-03-29 12:15:03 +0100

    feat: add AOS library for scroll animations and smooth scrolling functionality

--- Webuwka/laby04/src/js/main.js
+++ Webuwka/laby04/src/js/main.js
@@ -1,6 +1,28 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 import * as mdb from "mdb-ui-kit"; // lib
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 window.bootstrap = bootstrap;
 window.mdb = mdb;
+
+AOS.init({
+  duration: 300,
+  easing: "ease-in-out",
+  once: true,
+  mirror: false,
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      window.scrollTo({
+        top: target.offsetTop - 70,
+        behavior: "smooth",
+      });
+    }
+  });
+});
